dae0053db864fdaec5d8978236580a6af925ace1
Manuel Gardon <gardon@fktech.net>
2024-03-05 18:18:31 -0300

    First commit: tasks api

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto, UpdateTaskDto } from './dto/task.dto';
+
+// Metodos para la ruta tasks
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() newTask: CreateTaskDto) {
+    return this.tasksService.createTask(newTask.title, newTask.description);
+  }
+
+  @Delete(':id')
+  deleteTask(@Param('id') id: string) {
+    this.tasksService.deleteTask(id);
+  }
+
+  @Patch(':id')
+  updateTask(@Param('id') id: string, @Body() updatedFields: UpdateTaskDto) {
+    return this.tasksService.updateTask(id, updatedFields);
+  }
+}
